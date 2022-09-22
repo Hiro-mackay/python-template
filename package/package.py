6904656c63dd581ded05f7c38800f5e5aa6beeb6
mackay <johnmackay150@gmail.com>
2022-09-22 17:18:29 +0900

    init

---
+++ package/package.py
@@ -0,0 +1,2 @@
+def package():
+  return "Hello"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="",
+  version="1.0.0",
+  description="",
+  author="",
+  author_email="",
+  url=""
+)
